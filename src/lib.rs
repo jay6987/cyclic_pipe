d76a3ac8107cdc74fa27a1fab9d0eba97794f362
zhiwei <jay6987@hotmail.com>
2025-02-20 11:41:27 +0800

    * add LICENSE

--- src/lib.rs
+++ src/lib.rs
@@ -36,7 +36,6 @@
 //! ## Example
 //!
 //! ```rust
-//! use cyclic_pipe::Builder;
 //! use std::thread;
 //!
 //! fn main() {
@@ -91,7 +90,15 @@
 //!
 //! ## License
 //!
-//! This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
+//! This project is licensed under
+//!
+//! - [Apache License, Version 2.0](http://www.apache.org/licenses/LICENSE-2.0)
+//! - [MIT license](http://opensource.org/licenses/MIT)
+//!
+//! at your option.
+//!
+//! ## Contribution
+//! Unless you explicitly state otherwise, any contribution intentionally submitted for inclusion in the work by you, as defined in the Apache-2.0 license, shall be dual licensed as above, without any additional terms or conditions.
 
 #![warn(missing_docs)]
 
